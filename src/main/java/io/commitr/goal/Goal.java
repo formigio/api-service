71531099aa1fcbe136d95b490dcd89d5413d3f93
Peter Douglas <peter@thehouseofdouglas.com>
2016-09-05 14:17:39 -0600

    remove id field from json payloads

--- src/main/java/io/commitr/goal/Goal.java
+++ src/main/java/io/commitr/goal/Goal.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Data;
 
 @Data
@@ -11,6 +13,7 @@ import lombok.Data;
 public class Goal {
 	@Id
 	@GeneratedValue
+	@JsonIgnore
 	private Long id;
 	private String guid;
 	private String title;
