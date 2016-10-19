a8d5a802c7a6c098de282e5a369061f6e478c29a
Peter Douglas <peterfdouglas@ldschurch.org>
2016-10-19 02:19:23 -0600

    refactor to allow for additional providers

--- src/test/java/io/commitr/goal/GoalControllerTest.java
+++ src/test/java/io/commitr/goal/GoalControllerTest.java
@@ -3,20 +3,43 @@ package io.commitr.goal;
 import io.commitr.util.DTOUtils;
 import io.commitr.util.JsonUtils;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Request;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import java.io.Serializable;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.hamcrest.Matchers.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -33,6 +56,8 @@ public class GoalControllerTest {
     private final Goal GOAL_REQUEST = Goal.of(null, "Goal Test", DTOUtils.VALID_UUID);
 
     @Autowired
+    private WebApplicationContext context;
+
     private MockMvc mvc;
 
     @MockBean
@@ -40,6 +65,10 @@ public class GoalControllerTest {
 
     @Before
     public void setUp() throws Exception {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
 
         given(this.service.createGoal(GOAL_REQUEST))
                 .willReturn(GOAL_RESPONSE);
@@ -107,5 +136,49 @@ public class GoalControllerTest {
         this.mvc.perform(get("/goal?team=invalid-uuid-format")
                 .accept(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(status().is4xxClientError());
+    }
+
+    private Authentication getOauthTestAuthentication() {
+        return new OAuth2Authentication(getOauth2Request(), getAuthentication());
+    }
+    private OAuth2Request getOauth2Request () {
+        String clientId = "oauth-client-id";
+        Map<String, String> requestParameters = Collections.emptyMap();
+        boolean approved = true;
+        String redirectUrl = "http://my-redirect-url.com";
+        Set<String> responseTypes = Collections.emptySet();
+        Set<String> scopes = Collections.emptySet();
+        Set<String> resourceIds = Collections.emptySet();
+        Map<String, Serializable> extensionProperties = Collections.emptyMap();
+        List<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList("Everything");
+
+        OAuth2Request oAuth2Request = new OAuth2Request(requestParameters, clientId, authorities,
+                approved, scopes, resourceIds, redirectUrl, responseTypes, extensionProperties);
+
+        return oAuth2Request;
+    }
+
+    private Authentication getAuthentication() {
+        List<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList("Everything");
+
+        User userPrincipal = new User("user", "", true, true, true, true, authorities);
+
+        HashMap<String, String> details = new HashMap<String, String>();
+        details.put("user_name", "bwatkins");
+        details.put("email", "bwatkins@test.org");
+        details.put("name", "Brian Watkins");
+
+        TestingAuthenticationToken token = new TestingAuthenticationToken(userPrincipal, null, authorities);
+        token.setAuthenticated(true);
+        token.setDetails(details);
+
+        return token;
+    }
+
+    private OAuth2ClientContext getOauth2ClientContext () {
+        OAuth2ClientContext mockClient = mock(OAuth2ClientContext.class);
+        when(mockClient.getAccessToken()).thenReturn(new DefaultOAuth2AccessToken("my-fun-token"));
+
+        return mockClient;
     }
 }
