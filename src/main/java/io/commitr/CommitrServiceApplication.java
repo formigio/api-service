0005cfc6042471ef664c6a3afcbbbbcf34edd86d
Peter Douglas <peter@thehouseofdouglas.com>
2016-11-12 08:42:36 -0700

    Set pre-condition for x-identity-header, return 412

--- src/main/java/io/commitr/CommitrServiceApplication.java
+++ src/main/java/io/commitr/CommitrServiceApplication.java
@@ -2,12 +2,34 @@ package io.commitr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.WebMvcProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class CommitrServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommitrServiceApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver slr = new SessionLocaleResolver();
+		slr.setDefaultLocale(Locale.US); // Set default Locale as US
+		return slr;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+		source.setBasenames("i18n/messages");  // name of the resource bundle
+		source.setUseCodeAsDefaultMessage(true);
+		return source;
 	}
 }
--- src/main/java/io/commitr/configuration/WebConfiguration.java
+++ src/main/java/io/commitr/configuration/WebConfiguration.java
@@ -2,11 +2,15 @@ package io.commitr.configuration;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Objects;
 
@@ -23,6 +27,9 @@ public class WebConfiguration {
 
     class IdentityIdFilter implements Filter {
 
+        @Autowired
+        private MessageSource messageSource;
+
         private final Logger logger = LoggerFactory.getLogger(WebConfiguration.class);
 
         @Override
@@ -33,13 +40,22 @@ public class WebConfiguration {
         @Override
         public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
-            String identityHeader = ((HttpServletRequest)request).getHeader("x-identity-id");
+            if (!(request instanceof HttpServletRequest) || !(response instanceof HttpServletResponse)) {
+                throw new ServletException("Identity Filter just supports HTTP requests");
+            }
+
+            HttpServletRequest req = (HttpServletRequest)request;
+            HttpServletResponse res = (HttpServletResponse)response;
+
+
+            String identityHeader = req.getHeader("x-identity-id");
 
             if(!Objects.isNull(identityHeader)) {
                 logger.info(identityHeader);
+                chain.doFilter(request, response);
+            } else {
+                res.sendError(HttpServletResponse.SC_PRECONDITION_FAILED, messageSource.getMessage("missing.header",null,req.getLocale()));
             }
-
-            chain.doFilter(request, response);
         }
 
         @Override
