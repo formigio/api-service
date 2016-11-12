8b820c0a1ac4237f34963c8b887c527252f1849a
Peter Douglas <peter@thehouseofdouglas.com>
2016-11-12 08:45:31 -0700

    Move resource config to seperate class

--- src/main/java/io/commitr/CommitrServiceApplication.java
+++ src/main/java/io/commitr/CommitrServiceApplication.java
@@ -18,18 +18,4 @@ public class CommitrServiceApplication {
 		SpringApplication.run(CommitrServiceApplication.class, args);
 	}
 
-	@Bean
-	public LocaleResolver localeResolver() {
-		SessionLocaleResolver slr = new SessionLocaleResolver();
-		slr.setDefaultLocale(Locale.US); // Set default Locale as US
-		return slr;
-	}
-
-	@Bean
-	public ResourceBundleMessageSource messageSource() {
-		ResourceBundleMessageSource source = new ResourceBundleMessageSource();
-		source.setBasenames("i18n/messages");  // name of the resource bundle
-		source.setUseCodeAsDefaultMessage(true);
-		return source;
-	}
 }
